486a7f92c5414c24386b5a6d70ad2669d0b77457
labelnolongerexists <labelnolongerexists@gmail.com>
2013-11-14 10:12:11 +0800

    Fixed error in deploy.sh.

--- qm/bin/deploy.sh
+++ qm/bin/deploy.sh
@@ -4,6 +4,7 @@ line="############################################"
 code_home=/home/hadoop/git_project_home/XA-QueryMaster
 # Java home
 java_home=/usr/java/jdk1.7.0_25
+export JAVA_HOME=/usr/java/jdk1.7.0_25
 # Branch
 if [ "" = "$2" ]
 then
@@ -31,6 +32,7 @@ fi
 
 echo "[CHECK-POINT] - Begin deploying data driller web interface."
 echo ${line}
+echo "[JAVA-HOME] - "${JAVA_HOME}
 echo "[CODE-HOME] - "${code_home}
 echo "[TOMCAT] - "${tomcat_home}
 echo "[XA_ENV] - "${xa_env}
