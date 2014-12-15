6ccd5eb0e5aac6d7938b1dd87cf541ceb73a5860
griffinl <lq0987@126.com>
2014-12-15 14:15:07 +0800

    update

--- qm/src/main/java/com/xingcloud/qm/service/PlanExecutor.java
+++ qm/src/main/java/com/xingcloud/qm/service/PlanExecutor.java
@@ -29,8 +29,8 @@ public class PlanExecutor {
   //for PlanRunner. 
   private static final ExecutorService planExecutor =
     new ThreadPoolExecutor(
-      24,
-      24,
+      64,
+      64,
       30,
       TimeUnit.MINUTES,
       new ArrayBlockingQueue<Runnable>(256),
@@ -40,8 +40,8 @@ public class PlanExecutor {
   //for drillbitRunner.
   private static final ExecutorService drillBitExecutor =
     new ThreadPoolExecutor(
-      24,
-      24,
+      128,
+      128,
       30,
       TimeUnit.MINUTES,
       new ArrayBlockingQueue<Runnable>(256),
