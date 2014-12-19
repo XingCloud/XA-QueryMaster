2e3f51d2f16aa21be05eb42545c4780b251e5ae3
griffinl <lq0987@126.com>
2014-12-19 09:45:48 +0800

    update

--- qm/src/main/java/com/xingcloud/qm/service/QueryMaster.java
+++ qm/src/main/java/com/xingcloud/qm/service/QueryMaster.java
@@ -45,7 +45,7 @@ public class QueryMaster implements QueryListener {
 
   //每个project，同时最多允许多少个plan执行
 //  public static int MAX_PLAN_PER_PROJECT = 1;
-  public static int MAX_PLAN_PER_PROJECT = 2;
+  public static int MAX_PLAN_PER_PROJECT = 1;
 
   //最多允许多少个plan一起合并
 //  public static int MAX_BATCHMERGE = Integer.MAX_VALUE;
